1e8198e12dba49213928df1d93998ae0c6ba8ef8
Matey-Nikolov <matey.nikolov@mail.bg>
2023-03-13 13:01:11 +0200

    update

---
+++ DevDatesAPI/Controllers/SexualPreferenceController.cs
@@ -0,0 +1,61 @@
+﻿using DevDates.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace DevDatesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SexualPreferenceController
+    {
+        private readonly SexualPreference[] SexualPreference =
+        {
+            new SexualPreference()
+            {
+                DisplayName = "Men",
+                Photos = new []
+                {
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    },
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    },
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    }
+                }
+            },
+
+            new SexualPreference()
+            {
+                DisplayName = "Women",
+                Photos = new []
+                {
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    },
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    },
+                    new Photo()
+                    {
+                        url = "https://picsum.photos/200/300"
+                    }
+                }
+            }
+        };
+
+        [HttpGet("preference/{id}", Name = "GetPreference")]
+        public SexualPreference GetPreference()
+        {
+            Random rng = new Random();
+            return SexualPreference[rng.Next(SexualPreference.Length)];
+        }
+    }
+}
