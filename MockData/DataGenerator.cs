679f9aa5f2498e6e2bb361faadbfbe3ca3e6e6fc
Boyan MIlenkov <shad0w312443@gmail.com>
2023-03-27 12:23:56 +0300

    MockData in progress

---
+++ MockData/DataGenerator.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MockData.Person;
+
+namespace MockData
+{
+    public class DataGenerator
+    {
+        
+        public static Faker<Person> GetPersonGenerator()
+        {
+            return new Faker<Person>()
+                .RuleFor(e => e.FirstName, f => f.Name.FirstName())
+                .RuleFor(e => e.LastName, f => f.Name.LastName())
+                .RuleFor(e => e.DateFBirth, f => f.Person.DateOfBirth)
+                .RuleFor(e => e.GenderSelection, f => f.Person.Gender.ToString())
+                .RuleFor(e => e.Bio, f => f.Lorem.Paragraph(1))
+                .RuleFor(e => e.Email, (f, e) => f.Internet.Email(e.FirstName, e.LastName))
+                /*.RuleFor(e => e.Created, f => f.PickRandom)*/
+                .RuleFor(e => e.Modified, f => f.Date.Recent())
+                .RuleFor(e => e.ModifiedBy, (f, e) => f.Name.FirstName());
+
+        }
+        public static readonly List<Person> Persons = new List<Person>();
+        public const int NumOfPeople = 7;
+        public static void ResultData()
+        {
+            var generated = GetPersonGenerator();
+            var generatedU = generated.Generate(NumOfPeople);
+            Persons.AddRange(generatedU);
+        }
+    }
+}
---
+++ MockData/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MockData
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateFBirth { get; set; }
+        public string GenderSelection { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public string ModifiedBy { get; set; }
+
+        
+        public override string ToString()
+        {
+            return System.Text.Json.JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+        }
+    }
+}
