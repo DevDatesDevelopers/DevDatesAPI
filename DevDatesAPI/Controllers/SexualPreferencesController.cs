29beee095dd5d314cfec1a1c7e42d01baeac9105
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2023-03-29 03:06:03 +0300

    Ig някакви подобрения по SexualPreferencesController?
    Реално не знам как точно се прави това, но поне е нещо 🤷🏼‍♂️

--- DevDatesAPI/Controllers/SexualPreferencesController.cs
+++ DevDatesAPI/Controllers/SexualPreferencesController.cs
@@ -8,7 +8,7 @@ namespace DevDatesAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class SexualPreferencesController
+    public class SexualPreferencesController : ControllerBase
     {
         private DevDatesDbContext _context;
         public SexualPreferencesController(DevDatesDbContext context)
@@ -16,45 +16,103 @@ namespace DevDatesAPI.Controllers
             _context = context;
         }
         
+
+
         [HttpGet("preferences", Name = "GetAllPreferences")]
         public SexualPreference[] GetAllPreferences()
         {
             return _context.Genders.Select(g => new SexualPreference()
             {
+                Id = g.Id,
                 DisplayName = g.DisplayName
             }).ToArray();
         }
+
+
 
         [HttpGet("preference/{id}", Name = "GetPreference")]
         public SexualPreference GetPreference(int id)
         {
             return _context.Genders.Where(g => g.Id == id).Select(g => new SexualPreference()
             {
+                Id = g.Id,
                 DisplayName = g.DisplayName
             }).ToArray()[0];
         }
 
+
+
         [HttpDelete("preference/delete/{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            /*
             var context = _context.Genders.Where(g => g.Id == id);
 
             _context.Remove(context);
             _context.SaveChanges();
+            */
+
+            var context = _context.Genders.FirstOrDefault(g => g.Id == id);
+
+            if (context == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(context);
+            _context.SaveChanges();
+
+            return NoContent();
         }
+
+
 
         [HttpPost("preference/post/{id}")]
-        public void Post()
+        public IActionResult Post([FromBody] SexualPreference preference)
         {
             //var context = _context.Genders.Find(g => g.Id == id);
-            // TODO
+
+            if (preference == null)
+            {
+                return BadRequest();
+            }
+
+            var gender = new Gender
+            {
+                DisplayName = preference.DisplayName
+            };
+
+            _context.Genders.Add(gender);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetPreference", new { id = gender.Id }, preference);
         }
 
+
+
         [HttpPut("preference/put/{id}")]
-        public void put(int id, [FromBody] SexualPreference preference)
+        public IActionResult put(int id, [FromBody] SexualPreference preference)
         {
             //var context = _context.Genders.Find(g => g.Id == id);
-            //TODO
+            
+            if (preference == null || id != preference.Id)
+            {
+                return BadRequest();
+            }
+
+            var gender = _context.Genders.FirstOrDefault(g => g.Id == id);
+
+            if (gender == null)
+            {
+                return NotFound();
+            }
+
+            gender.DisplayName = preference.DisplayName;
+
+            _context.Genders.Update(gender);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
