51d8ff004600235630ddd0951723591cfb4ff032
Alexander Boev <naidobrixxjr@gmail.com>
2023-04-24 13:19:05 +0300

    Initial commit

--- MockDataGenerator/Program.cs
+++ MockDataGenerator/Program.cs
@@ -21,20 +21,46 @@ class Program
         Console.WriteLine("Finished generating mock data");
     }
 
+    //static async System.Threading.Tasks.Task InsertGenders(SqlConnection connection)
+    //{
+    //    var genders = new List<ShortUserInfo> {
+    //        new ShortUserInfo { Gender = "Male" },
+    //        new ShortUserInfo{ Gender = "Female" },
+    //        new ShortUserInfo{ Gender = "Non-binary" }
+    //    };
+
+    //    foreach (var gender in genders)
+    //    {
+    //        try
+    //        {
+    //            await using var command = new SqlCommand("INSERT INTO Genders (Id, DisplayName, Created, Modified, ModifiedBy) VALUES (/*@id,*/ @displayName, GETDATE(), GETDATE(), 'Admin')", connection);
+    //            //command.Parameters.AddWithValue("@id", Guid.NewGuid());
+    //            command.Parameters.AddWithValue("@displayName", gender.Gender);
+    //            await command.ExecuteNonQueryAsync();
+    //            Console.WriteLine($"Inserted gender {gender.Gender}");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //    }
+    //}
     static async System.Threading.Tasks.Task InsertGenders(SqlConnection connection)
     {
         var genders = new List<ShortUserInfo> {
-            new ShortUserInfo { Gender = "Male" },
-            new ShortUserInfo{ Gender = "Female" },
-            new ShortUserInfo{ Gender = "Non-binary" }
-        };
+        new ShortUserInfo { Gender = "Male" },
+        new ShortUserInfo{ Gender = "Female" },
+        new ShortUserInfo{ Gender = "Non-binary" }
+    };
 
         foreach (var gender in genders)
         {
             try
             {
-                await using var command = new SqlCommand("INSERT INTO Genders (/*Id,*/ DisplayName, Created, Modified, ModifiedBy) VALUES (/*@id,*/ @displayName, GETDATE(), GETDATE(), 'Admin')", connection);
-                //command.Parameters.AddWithValue("@id", Guid.NewGuid());
+                var id = Guid.NewGuid();
+
+                await using var command = new SqlCommand("INSERT INTO Genders (Id, DisplayName, Created, Modified, ModifiedBy) VALUES (@id, @displayName, GETDATE(), GETDATE(), 'Admin')", connection);
+                command.Parameters.AddWithValue("@id", id);
                 command.Parameters.AddWithValue("@displayName", gender.Gender);
                 await command.ExecuteNonQueryAsync();
                 Console.WriteLine($"Inserted gender {gender.Gender}");
@@ -46,23 +72,65 @@ class Program
         }
     }
 
+
+    //static async System.Threading.Tasks.Task InsertUsers(SqlConnection connection)
+    //{
+    //    var genders = new List<int>();
+
+    //    await using (var command = new SqlCommand("SELECT Id FROM Genders", connection))
+    //    {
+    //        await using var reader = await command.ExecuteReaderAsync();
+    //        while (await reader.ReadAsync())
+    //        {
+    //            genders.Add(reader.GetInt32(0));
+    //        }
+    //    }
+
+    //    var faker = new Faker();
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        //var id = Guid.NewGuid();
+    //        var name = faker.Name.FullName();
+    //        var dob = faker.Date.Past(50);
+    //        var genderId = faker.Random.ArrayElement(genders.ToArray());
+    //        var bio = faker.Lorem.Sentence();
+    //        var email = faker.Internet.Email();
+
+    //        try
+    //        {
+    //            await using var command = new SqlCommand("INSERT INTO Users (Id, Name, DateOfBirth, GenderId, Bio, Email, Created, Modified, ModifiedBy) VALUES (/*@id,*/ @name, @dob, @genderId, @bio, @email, GETDATE(), GETDATE(), 'Admin')", connection);
+    //            //command.Parameters.AddWithValue("@id", id);
+    //            command.Parameters.AddWithValue("@name", name);
+    //            command.Parameters.AddWithValue("@dob", dob);
+    //            command.Parameters.AddWithValue("@genderId", genderId);
+    //            command.Parameters.AddWithValue("@bio", bio);
+    //            command.Parameters.AddWithValue("@email", email);
+    //            await command.ExecuteNonQueryAsync();
+    //            Console.WriteLine($"Inserted user {name}");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //    }
+    //}
     static async System.Threading.Tasks.Task InsertUsers(SqlConnection connection)
     {
-        var genders = new List<int>();
+        var genders = new List<Guid>();
 
         await using (var command = new SqlCommand("SELECT Id FROM Genders", connection))
         {
             await using var reader = await command.ExecuteReaderAsync();
             while (await reader.ReadAsync())
             {
-                genders.Add(reader.GetInt32(0));
+                genders.Add(reader.GetGuid(0));
             }
         }
 
         var faker = new Faker();
         for (int i = 0; i < 10; i++)
         {
-            //var id = Guid.NewGuid();
+            var id = Guid.NewGuid();
             var name = faker.Name.FullName();
             var dob = faker.Date.Past(50);
             var genderId = faker.Random.ArrayElement(genders.ToArray());
@@ -71,8 +139,8 @@ class Program
 
             try
             {
-                await using var command = new SqlCommand("INSERT INTO Users (/*Id,*/ Name, DateOfBirth, GenderId, Bio, Email, Created, Modified, ModifiedBy) VALUES (/*@id,*/ @name, @dob, @genderId, @bio, @email, GETDATE(), GETDATE(), 'Admin')", connection);
-                //command.Parameters.AddWithValue("@id", id);
+                await using var command = new SqlCommand("INSERT INTO Users (Id, Name, DateOfBirth, GenderId, Bio, Email, Created, Modified, ModifiedBy) VALUES (@id, @name, @dob, @genderId, @bio, @email, GETDATE(), GETDATE(), 'Admin')", connection);
+                command.Parameters.AddWithValue("@id", id);
                 command.Parameters.AddWithValue("@name", name);
                 command.Parameters.AddWithValue("@dob", dob);
                 command.Parameters.AddWithValue("@genderId", genderId);
@@ -87,6 +155,7 @@ class Program
             }
         }
     }
+
 }
 
 
@@ -94,5 +163,5 @@ class Program
 
 //Access to the specified database with appropriate permissions to create tables and insert data.
 //The required tables Genders and Users exist in the specified database with the required columns.
-//A valid connection string to a Microsoft SQL Server database.fied database with the required columns.
+//The required tables Genders and Users exist in the specified database with the required columns.
 //The necessary .NET libraries are installed and available on your system to compile and run the code.
