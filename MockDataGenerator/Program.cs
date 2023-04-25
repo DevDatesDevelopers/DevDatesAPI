72083f62c321e70f08123702c37fd8d7912d8b41
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-25 17:36:38 +0300

    update
    Ако правилно сме разбрали това така ли трябва да се напише?
    (Ако е така останалата част ще я напише Боян Миленков)
    Приятен ден!

--- MockDataGenerator/Program.cs
+++ MockDataGenerator/Program.cs
@@ -156,6 +156,181 @@ class Program
         }
     }
 
+    static async System.Threading.Tasks.Task InsertResourcesTypes(SqlConnection connection)
+    {
+        var resourcestypes = new List<Guid>();
+
+        var faker = new Faker();
+        for (int i = 0; i < 10; i++)
+        {
+            var id = Guid.NewGuid();
+            var displayName = faker.Name.FullName();
+
+            try
+            {
+                await using var command = new SqlCommand("INSERT INTO ResourcesTypes (Id, Name, Created, Modified, ModifiedBy) VALUES (@id, @displayName, GETDATE(), GETDATE(), 'Admin')", connection);
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@displayName", displayName);
+                await command.ExecuteNonQueryAsync();
+                Console.WriteLine($"Inserted resources types.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static async System.Threading.Tasks.Task InsertResources(SqlConnection connection)
+    {
+        var resources = new List<Guid>();
+
+        await using (var command = new SqlCommand("SELECT Id FROM ResourcesTypes", connection))
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                resources.Add(reader.GetGuid(0));
+            }
+        }
+
+        var faker = new Faker();
+        for (int i = 0; i < 10; i++)
+        {
+            var id = Guid.NewGuid();
+            var resourceURI = faker.Internet.Url();
+            var resourceTypeId = faker.Random.ArrayElement(resources.ToArray());
+
+            try
+            {
+                await using var command = new SqlCommand("INSERT INTO ResourcesTypes (Id, ResourceURI, ResourceTypeId) VALUES (@id, @url, @typeId)", connection);
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@url", resourceURI);
+                command.Parameters.AddWithValue("@typeId", resourceTypeId);
+                await command.ExecuteNonQueryAsync();
+                Console.WriteLine($"Inserted resources.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static async System.Threading.Tasks.Task InsertInterests(SqlConnection connection)
+    {
+        var faker = new Faker();
+        for (int i = 0; i < 10; i++)
+        {
+            var id = Guid.NewGuid();
+            var displayName = faker.Name.FullName();
+
+            try
+            {
+                await using var command = new SqlCommand("INSERT INTO ResourcesTypes (Id, ResourceURI, ResourceTypeId, Created, Modified, ModifiedBy) VALUES (@id, @name, GETDATE(), GETDATE(), 'Admin')", connection);
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@name", displayName);
+                await command.ExecuteNonQueryAsync();
+                Console.WriteLine($"Inserted interests.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static async System.Threading.Tasks.Task InterestsResources(SqlConnection connection)
+    {
+        var resourcesInsert = new List<Guid>();
+
+        await using (var command = new SqlCommand("SELECT Id FROM Interests", connection))
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                resourcesInsert.Add(reader.GetGuid(0));
+            }
+        }
+
+        var resourcesId = new List<Guid>();
+
+        await using (var command = new SqlCommand("SELECT Id FROM Resources", connection))
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                resourcesId.Add(reader.GetGuid(0));
+            }
+        }
+
+        var faker = new Faker();
+        for (int i = 0; i < 10; i++)
+        {
+            var interestId = faker.Random.ArrayElement(resourcesInsert.ToArray());
+            var resourceId = faker.Random.ArrayElement(resourcesId.ToArray());
+            //TODO
+            //PRIMARY KEY(InterestId, ResourceId) - how?
+            try
+            {
+                await using var command = new SqlCommand("INSERT INTO ResourcesTypes (InterestId, ResourceId, ResourceTypeId) VALUES (@id, @resourceId)", connection);
+                command.Parameters.AddWithValue("@id", interestId);
+                command.Parameters.AddWithValue("@resourceId", resourceId);
+                await command.ExecuteNonQueryAsync();
+                Console.WriteLine($"Inserted resources.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static async System.Threading.Tasks.Task InterestsUsersResources(SqlConnection connection)
+    {
+        var users = new List<Guid>();
+
+        await using (var command = new SqlCommand("SELECT Id FROM Users", connection))
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                users.Add(reader.GetGuid(0));
+            }
+        }
+
+        var resourcesId = new List<Guid>();
+
+        await using (var command = new SqlCommand("SELECT Id FROM Resources", connection))
+        {
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                resourcesId.Add(reader.GetGuid(0));
+            }
+        }
+
+        var faker = new Faker();
+        for (int i = 0; i < 10; i++)
+        {
+            var userId = faker.Random.ArrayElement(users.ToArray());
+            var resourceId = faker.Random.ArrayElement(resourcesId.ToArray());
+            //TODO
+            //PRIMARY KEY (UserId, ResourceId), - how?
+            try
+            {
+                await using var command = new SqlCommand("INSERT INTO ResourcesTypes (InterestId, ResourceId, ResourceTypeId) VALUES (@id, @resourceId)", connection);
+                command.Parameters.AddWithValue("@id", userId);
+                command.Parameters.AddWithValue("@resourceId", resourceId);
+                await command.ExecuteNonQueryAsync();
+                Console.WriteLine($"Inserted users resources.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
 }
 
 
