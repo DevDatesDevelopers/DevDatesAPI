6139345c46eb07366fe601f46df02edafd21fa8e
Blagovest-Hristov <bobo4e.h@gmail.com>
2023-05-10 09:43:35 +0300

    blago :)

--- DevDates API Wrapper/Wrapper.cs
+++ DevDates API Wrapper/Wrapper.cs
@@ -86,8 +86,213 @@ namespace DevDatesApiWrapper
             var json = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<Photo>(json);
         }
+
         
+        public async Task<User> PostUser(int id)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<User>(json);
+
+        }
+
+        public async Task<Photo> PostPhoto(int id, int photoId)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/photos/{photoId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Photo>(json);
+        }
+
+        public async Task<Interest> PostInterest(int id, int interestId)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/interests/{interestId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Interest>(json);
+        }
+
+        public async Task<ConnectedService> PostConnectedService(int id, int connectedServiceId)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/connected-services/{connectedServiceId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ConnectedService>(json);
+        }
+
+        public async Task<SexualPreference> PostSexualPreference(int id, int sexualPreferenceId)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/sexual-preferences/{sexualPreferenceId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<SexualPreference>(json);
+        }
+
+        public async Task<ShortUserInfo> PostShortUserInfo(int id)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/short-info", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ShortUserInfo>(json);
+        }
+
+        public async Task<DetailedUserInfo> PostDetailedUserInfo(int id)
+        {
+            var response = await _httpClient.PostAsync($"{_baseUrl}users/{id}/detailed-info", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<DetailedUserInfo>(json);
+        }
+
+        public async Task<User> PutUser(int id)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<User>(json);
+
+        }
+
+        public async Task<Photo> PutPhoto(int id, int photoId)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/photos/{photoId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Photo>(json);
+        }
+
+        public async Task<Interest> PutInterest(int id, int interestId)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/interests/{interestId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Interest>(json);
+        }
+
+        public async Task<ConnectedService> PutConnectedService(int id, int connectedServiceId)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/connected-services/{connectedServiceId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ConnectedService>(json);
+        }
+
+        public async Task<SexualPreference> PutSexualPreference(int id, int sexualPreferenceId)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/sexual-preferences/{sexualPreferenceId}", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<SexualPreference>(json);
+        }
+
+        public async Task<ShortUserInfo> PutShortUserInfo(int id)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/short-info", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ShortUserInfo>(json);
+        }
+
+        public async Task<DetailedUserInfo> PutDetailedUserInfo(int id)
+        {
+            var response = await _httpClient.PutAsync($"{_baseUrl}users/{id}/detailed-info", null);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<DetailedUserInfo>(json);
+        }
+
+        public async Task<User> DeleteUser(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<User>(json);
+        }
+
+        public async Task<Photo> DeletePhoto(int id, int photoId)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/photos/{photoId}");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Photo>(json);
+        }
+
+        public async Task<Interest> DeleteInterest(int id, int interestId)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/interests/{interestId}");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Interest>(json);
+        }
+
+        public async Task<ConnectedService> DeleteConnectedService(int id, int connectedServiceId)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/connected-services/{connectedServiceId}");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ConnectedService>(json);
+        }
+
+        public async Task<SexualPreference> DeleteSexualPreference(int id, int sexualPreferenceId)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/sexual-preferences/{sexualPreferenceId}");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<SexualPreference>(json);
+        }
+
+        public async Task<ShortUserInfo> DeleteShortUserInfo(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/short-info");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ShortUserInfo>(json);
+        }
+
+        public async Task<DetailedUserInfo> DeleteDetailedUserInfo(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/detailed-info");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<DetailedUserInfo>(json);
+        }
+
+        public async Task<User> DeleteAll(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}users/{id}/all");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<User>(json);
+        }
+
         
+
+
+
+
 
         public class User
         {
